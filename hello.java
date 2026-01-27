17a166521a77ef825500e720e2117346891f361e
Aditya Shirwalkar <aditya.shirwalkar@rajasoftwarelabs.com>
2026-01-27 13:01:05 +0530

    removed feature 2

--- hello.java
+++ hello.java
@@ -2,7 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, World from branch feature1");
-        System.out.println("Hello, World from branch feature2");
         System.out.println("New feature1 change");
         System.out.println("New feature1 to be added here");
         System.out.println("New feature1 to be added here again");
