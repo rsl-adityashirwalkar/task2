4b15598f77881eb4ac44c6647ce337454fae5434
Aditya Shirwalkar <aditya.shirwalkar@rajasoftwarelabs.com>
2026-01-27 12:23:54 +0530

    Deleted recent change

--- hello.java
+++ hello.java
@@ -4,6 +4,5 @@ public class hello {
         System.out.println("Hello, World from branch feature1");
         System.out.println("New feature1 to be added here");
         System.out.println("New feature1 to be added here again");
-        System.out.println("test");
     }
 }
